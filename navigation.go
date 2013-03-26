07d397eaa2f07a33389863620d473b2cde752824
Ruben Pollan <meskio@sindominio.net>
2013-03-26 11:50:36 +0100

    Navigation cursor working

--- navigation.go
+++ navigation.go
@@ -4,16 +4,23 @@
 
 package epubgo
 
+import (
+	"errors"
+)
+
 type NavigationIterator struct {
-	depth  []navpoint
+	parents []navCursor
+	curr    navCursor
+}
+type navCursor struct {
 	navMap []navpoint
 	index  int
 }
 
 func newNavigationIterator(navMap []navpoint) *NavigationIterator {
 	var nav NavigationIterator
-	nav.navMap = navMap
-	nav.index = 0
+	nav.curr.navMap = navMap
+	nav.curr.index = 0
 	return &nav
 }
 
@@ -30,17 +37,52 @@ func (nav NavigationIterator) HasChildren() bool {
 }
 
 func (nav NavigationIterator) HasParents() bool {
-	return nav.depth != nil
+	return nav.parents != nil
 }
 
 func (nav NavigationIterator) IsFirst() bool {
-	return nav.index == 0
+	return nav.curr.index == 0
 }
 
 func (nav NavigationIterator) IsLast() bool {
-	return nav.index == len(nav.navMap)
+	return nav.curr.index == len(nav.curr.navMap)-1
+}
+
+func (nav *NavigationIterator) Next() error {
+	if nav.IsLast() {
+		return errors.New("It is the last entry")
+	}
+	nav.curr.index++
+	return nil
+}
+
+func (nav *NavigationIterator) Previous() error {
+	if nav.IsFirst() {
+		return errors.New("It is the first entry")
+	}
+	nav.curr.index--
+	return nil
+}
+
+func (nav *NavigationIterator) In() error {
+	if !nav.HasChildren() {
+		return errors.New("It has no children")
+	}
+	nav.parents = append(nav.parents, nav.curr)
+	nav.curr.navMap = nav.item().Children()
+	nav.curr.index = 0
+	return nil
+}
+
+func (nav *NavigationIterator) Out() error {
+	if !nav.HasParents() {
+		return errors.New("It has no parents")
+	}
+	nav.curr = nav.parents[len(nav.parents)-1]
+	nav.parents = nav.parents[:len(nav.parents)-1]
+	return nil
 }
 
 func (nav NavigationIterator) item() *navpoint {
-	return &nav.navMap[nav.index]
+	return &nav.curr.navMap[nav.curr.index]
 }
--- navigation_test.go
+++ navigation_test.go
@@ -9,6 +9,7 @@ import "testing"
 const (
 	first_title = "A DOG'S TALE, By Mark Twain"
 	first_url   = "@public@vhost@g@gutenberg@html@files@3174@3174-h@3174-h-0.htm.html#pgepubid00000"
+	child_title = "Frontpiece"
 )
 
 func TestIterator(t *testing.T) {
@@ -47,5 +48,51 @@ func TestUrl(t *testing.T) {
 	it := f.Navigation()
 	if it.Url() != first_url {
 		t.Errorf("it.Url() return: %v when was expected: %v", it.Url(), first_url)
+	}
+}
+
+func TestDepth(t *testing.T) {
+	f, _ := Open(book_path)
+	defer f.Close()
+
+	it := f.Navigation()
+	if it.In() == nil {
+		t.Errorf("it.In() din't return an error whithout having children")
+	}
+	if err := it.Next(); err != nil {
+		t.Errorf("it.Next() return an error: %v", err)
+	}
+	if err := it.Next(); err != nil {
+		t.Errorf("it.Next() return an error: %v", err)
+	}
+	if !it.IsLast() {
+		t.Errorf("it.IsLast() not behaving as expected")
+	}
+	if err := it.In(); err != nil {
+		t.Errorf("it.In() return an error: %v", err)
+	}
+	if it.Previous() == nil {
+		t.Errorf("it.Previous() din't return an error being the first")
+	}
+	if err := it.Next(); err != nil {
+		t.Errorf("it.Next() return an error: %v", err)
+	}
+	if err := it.Previous(); err != nil {
+		t.Errorf("it.Previous() return an error: %v", err)
+	}
+	if it.Title() != child_title {
+		t.Errorf("it.Title() return: %v when was expected: %v", it.Title(), child_title)
+	}
+	if err := it.Out(); err != nil {
+		t.Errorf("it.Out() return an error: %v", err)
+	}
+	if err := it.Previous(); err != nil {
+		t.Errorf("it.Next() return an error: %v", err)
+	}
+	if err := it.Previous(); err != nil {
+		t.Errorf("it.Next() return an error: %v", err)
+	}
+	if it.Title() != first_title {
+		t.Errorf("it.Title() return: %v when was expected: %v", it.Title(), first_title)
 	}
 }
